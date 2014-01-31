abacae932757ef560996a333066f205ae25ce3ce
John Stradling <john@selric13.com>
2014-01-31 12:42:27 -0600

    Remove old popup

--- mobile/MobilePopup.js
+++ mobile/MobilePopup.js
@@ -107,7 +107,7 @@
 		console.log("Smartbanner");
 		smartbanner();
 		
-    if (jQuery.browser.mobile) {
+    /*if (jQuery.browser.mobile) {
     	var timeoutID;
     	// Check for shoutz cookie
     	
@@ -142,7 +142,7 @@
             return false;
         });
     }	
-    
+    */
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
