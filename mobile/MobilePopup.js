c7cfdca9d88adfd11e4f1c15a14dd43054459aee
John Stradling <john@selric13.com>
2014-01-31 13:37:45 -0600

    rebuild #20

--- mobile/MobilePopup.js
+++ mobile/MobilePopup.js
@@ -105,7 +105,8 @@
 		//$('head').append('<link rel="stylesheet" href="http://shoutz-pbexptest.azurewebsites.net/mobile/jquery.smartbanner.css" type="text/css" media="screen" />');
 		//$('head').append('<script src="http://powerballexp.shoutz.com/mobile/jquery.smartbanner.js"></script>');
 		console.log("Smartbanner");
-		smartbanner();
+	
+	$(document).ready(smartbanner());
 		
     /*if (jQuery.browser.mobile) {
     	var timeoutID;
